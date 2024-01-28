371fd234b93cef4b7364098b3c4b1cc56ace08ed
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-01-28 01:32:05 -0300

    update increment build

---
+++ App.tsx
@@ -0,0 +1,81 @@
+// File: App.tsx
+
+import React from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View, Dimensions } from 'react-native';
+import Header from './components/Header'; // Import the custom Header component
+
+const { width } = Dimensions.get('window');
+
+const Step: React.FC<{ title: string; content: string }> = ({ title, content }) => {
+  return (
+    <View style={styles.stepContainer}>
+      <Text style={styles.stepTitle}>{title}</Text>
+      <Text style={styles.stepContent}>{content}</Text>
+    </View>
+  );
+};
+
+const App: React.FC = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? '#0E1D34' : '#FFFFFF', // Example of dark and light theme
+  };
+
+  return (
+    <SafeAreaView style={[styles.safeAreaView, backgroundStyle]}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Header />
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}
+      >
+        {/* Each Step component is a page in the horizontal scroll view */}
+        <Step
+          title="Step One"
+          content="Install Harness and configure your project for CI/CD pipelines."
+        />
+        <Step
+          title="Step Two"
+          content="Define your build and test workflows within the Harness platform."
+        />
+        <Step
+          title="Step Three"
+          content="Set up your deployment strategy and distribution methods for iOS."
+        />
+        <Step
+          title="Step Four"
+          content="Monitor your app's performance and user feedback after deployment."
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+  stepContainer: {
+    width: width, // Each step will be the full width of the screen
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  stepTitle: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#0292E3', // Harness brand color for titles
+    marginBottom: 15,
+  },
+  stepContent: {
+    fontSize: 18,
+    color: '#5C5C5C', // Subdued text color for content
+  },
+  // ... Add more styles as needed
+});
+
+export default App;
