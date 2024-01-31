dd64bcf3a0df37c9ffc10aa9d85f0f81a0ef8610
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-01-31 00:27:59 -0300

    jest junit config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   preset: 'react-native',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'test-results',
+        outputName: 'jest-junit.xml',
+      },
+    ],
+  ],
 };
